efeeb1a5cd88259bfc8897b49a1e959ec28d50eb
Nacho Colomina Torregrosa <ignacio.colomina@gmail.com>
2015-10-12 21:04:36 +0200

    Update UserNotificationsCommand.php

--- src/AppBundle/Console/UserNotificationsCommand.php
+++ src/AppBundle/Console/UserNotificationsCommand.php
@@ -37,7 +37,7 @@ class UserNotificationsCommand extends ContainerAwareCommand
             ->setSubject('Hello Email')
             ->setFrom('send@example.com')
             ->setTo('recipient@example.com')
-            ->setBody($this->renderView('Emails/registration.html.twig', array('notifications' => $notifications)), 'text/html')
+            ->setBody($this->renderView('AppBundle::NotificationEmail.html.twig', array('notifications' => $notifications)), 'text/html')
 
         ;
 

d8f6646dbdd3ed5fe828e38eade9239419694172
Ignacio Colomina <ignacio.colomina@gmail.com>
2015-10-12 21:18:46 +0200

    Ultimas modificaciones

---
+++ src/AppBundle/Command/UserNotificationsCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UserNotificationsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('user:send-notifications')
+            ->setDescription('Send user notifications')
+            ->addArgument('email', InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $email = $input->getArgument('email');
+
+        $notifications = $this->getContainer()->get('doctrine')->getRepository('AppBundle:Notification')
+             ->findReadyToSendByEmail($email);
+        ;
+
+        if(empty($notifications)){
+
+            $output->writeln('<info>There are no notifications for ' . $email . '</info>');
+            return;
+        }
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody($this->getContainer()->get('templating')->render('AppBundle::NotificationEmail.html.twig', array('notifications' => $notifications)), 'text/html')
+
+        ;
+
+        try {
+            $output->writeln('<info>Sending an email to ' . $email . ' with ' . count($notifications) . '</info>');
+
+            $this->getContainer->get('mailer')->send($message);
+            $output->writeln('<info>Email sent successfuly</info>');
+        }
+        catch(\Exception $e){
+
+            $output->writeln('<error>An error ocurred while sending the email. Please contect with the administrator</error>');
+        }
+    }
+}
--- src/AppBundle/Repository/NotificationRepository.php
+++ src/AppBundle/Repository/NotificationRepository.php
@@ -7,13 +7,14 @@ class NotificationRepository extends \Doctrine\ORM\EntityRepository
 
     public function findReadyToSendByEmail($email){
 
-        $date = \DateTime::createFromFormat('Y-m-d H:i:s', strtotime("-1 day"));
+        $date = date('Y-m-d H:i:s', strtotime("-1 day"));
 
         $qb = $this->createQueryBuilder('n');
         return $qb
-                ->where('n.email', ':email')
-                ->andWhere($qb->expr()->lte('n.createdAt', $date))
+                ->where($qb->expr()->eq('n.email', ':email'))
+                ->andWhere($qb->expr()->lte('n.createdAt', ':date'))
                 ->setParameter('email', $email)
+                ->setParameter('date', $date)
                 ->getQuery()
                 ->getResult()
         ;
